a620b08ad1b0cc2b8d400389a2cb8d640bab769e
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-18 00:09:04 -0300

    feat: implements card component

---
+++ src/components/Card/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const StyledCard = styled.div`
+  max-width: 400px;
+  width: 100%;
+  height: 200px;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  justify-content: center;
+  text-align: center;
+  padding: 40px 20px;
+  position: relative;
+  box-shadow: var(--shadowGrey);
+  background: var(--secundary);
+  color: var(--white);
+  &:hover{
+    transform: scale(1.04);
+    box-shadow: var(--shadowViolet);
+    transition: all 0.4s ease-in-out;
+  }
+`
+export const StyledTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+`
+export const StyledContent = styled.p`
+  font-size: 16px;
+`
