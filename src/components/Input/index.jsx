ce4a99133c1cc7e6adb9444ec9c287ac56383467
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-18 03:05:09 -0300

    feat: implements input component

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,15 @@
+import { StyledInput, StyledLabel, StyledLabelText } from "./style";
+
+export function Input() {
+  return(
+    <>
+    <StyledLabel>
+      <StyledLabelText>
+        Titulo
+      </StyledLabelText>
+      <StyledInput />
+    </StyledLabel>
+    
+    </>
+  )
+}
---
+++ src/components/Input/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledLabel = styled.label`
+ display: flex;
+ flex-direction: column;
+ gap: 8px;
+`
+export const StyledInput = styled.input`
+  width: 100%;
+  background-color: var(--background);
+  padding: 20px;
+  font-size: 16px;
+  color: var(--white);
+  outline: none;
+  border-radius: 6px;
+  border: none;
+`
+export const StyledLabelText = styled.span`
+  font-size: 16px;
+  color: var(--white);
+`
