11cea12986c3c875d6073af1061418d039624ea6
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-18 03:06:00 -0300

    feat: implements Posts component

--- src/components/Posts/styles.js
+++ src/components/Posts/styles.js
@@ -8,19 +8,3 @@ export const PostsSection = styled.section`
   gap: 28px;
   flex-wrap: wrap;
 `
-export const StyledIcon = styled.div`
-  display: flex;
-  align-items: center;
-  position: fixed;
-  bottom: 50px;
-  right: 50px;
-  background-color: var(--primary);
-  border-radius: 50%;
-  padding: 5px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  cursor: pointer;
-  &:hover{
-    box-shadow: var(--shadowViolet);
-    transition: all 0.4s ease-in-out;
-  }
-`
