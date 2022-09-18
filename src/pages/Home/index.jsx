d3127f5272776e10704ced38e76e0f61898bb872
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-18 00:11:08 -0300

    feat: implements home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
-
+import { Posts } from "../../components/Posts";
+import { Template } from "../../components/template";
 
 export function Home() {
 
   return(
-    <></>
+    <>
+    <Template>
+      <Posts />
+    </Template>
+    </>
   )
 }
