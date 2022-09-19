a1629838f44e0884a3cc66736bfaeb0bc5031a8e
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-18 21:58:13 -0300

    feat: implements input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,24 @@
-import { StyledInput, StyledLabel, StyledLabelText } from "./style";
+import { StyledInput, StyledTextarea } from "./style";
+import { StyledLabel, StyledLabelText } from "../../globalStyle";
 
-export function Input() {
+export function Input({title, type, name, handleChange}) {
   return(
     <>
-    <StyledLabel>
-      <StyledLabelText>
-        Titulo
-      </StyledLabelText>
-      <StyledInput />
-    </StyledLabel>
-    
+      <StyledLabel>
+        <StyledLabelText>
+          {title}
+        </StyledLabelText>
+      {
+        type == 'text' ?
+        <StyledInput
+          name={name}  
+          onChange={handleChange}/>
+        :
+        <StyledTextarea
+          name={name}  
+          onChange={handleChange}/>
+      } 
+      </StyledLabel>
     </>
   )
 }
--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 
-export const StyledLabel = styled.label`
- display: flex;
- flex-direction: column;
- gap: 8px;
-`
 export const StyledInput = styled.input`
   width: 100%;
   background-color: var(--background);
@@ -15,7 +10,15 @@ export const StyledInput = styled.input`
   border-radius: 6px;
   border: none;
 `
-export const StyledLabelText = styled.span`
+
+export const StyledTextarea = styled.textarea`
+  width: 100%;
+  height: 150px;
+  background-color: var(--background);
+  padding: 20px;
   font-size: 16px;
   color: var(--white);
+  outline: none;
+  border-radius: 6px;
+  border: none;
 `
