e178693a4086d35cd4c24330bf92bad78390feb0
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-25 13:35:05 -0300

    feat: implements post details page

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,9 +1,9 @@
 import { User } from "phosphor-react"
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { Template } from "../../components/template"
 import api from "../../services/api"
-import { StyledLink, StyledSection, StyledPostSection, StyledTitle, StyledDiv, StyledBody } from "./style"
+import { StyledLink, StyledSection, StyledPostSection, StyledTitle, StyledDiv, StyledBody, StyledIcon, StyledCommentName, StyledCommentBody } from "./style"
 
 export function Post() {
   const [post, setPost] = useState('')
@@ -32,13 +32,17 @@ export function Post() {
   return(
     <Template>
       <StyledDiv>
-        <StyledSection padding>
+        <StyledSection>
           <StyledLink target='_blank' to={`/users/${user.id}`}>
             <h2>
               {user.name}
             </h2>
-          </StyledLink>   
-          <User size={40}/>
+          </StyledLink>
+          <Link target='_blank' to={`/users/${user.id}`}>
+            <StyledIcon>
+              <User size={40}/> 
+            </StyledIcon>   
+          </Link>
         </StyledSection>
         <StyledPostSection>
           <StyledTitle>
@@ -49,11 +53,13 @@ export function Post() {
           </StyledBody>
           {
             comments.map(comment => (
-              <StyledSection key={comment.id} padding>
-                <>
-                  <p>{comment.name}</p>
-                  {/* <p>{comment.body}</p> */}
-                </>
+              <StyledSection key={comment.id} column gray>
+                <StyledCommentName>
+                  {comment.name}                  
+                </StyledCommentName>
+                <StyledCommentBody>
+                  {comment.body}
+                </StyledCommentBody>
               </StyledSection>
             ))
           }
--- src/pages/Post/style.js
+++ src/pages/Post/style.js
@@ -10,16 +10,27 @@ export const StyledDiv = styled.div`
 export const StyledLink = styled(Link)`
   color: var(--white);
   text-decoration: none;
+  &:hover {
+    color: var(--tertiary);
+    scale: 1.06;
+    transition: all 0.4s ease-in-out;
+  }  
 `
 export const StyledSection = styled.section`
   border-top: 1px solid var(--secundary);
   border-bottom: 1px solid var(--secundary);
   display: flex;
+  gap: 10px;
   justify-content: space-around;
-  align-items: center;  
-  padding: 
+  align-items: center; 
+  padding: 20px;
+  border-radius: 6px;
+  flex-direction: 
   ${
-    props => props.padding && '20px'
+    props => props.column && 'column'
+  };
+  background-color: ${
+    props => props.gray && 'var(--secundary)'
   };
 `
 
@@ -32,16 +43,41 @@ export const StyledPostSection = styled.section`
 
 export const StyledTitle = styled.h1`
   text-align: center;
+  align-self: center;
   font-size: 40px;
   letter-spacing: 2px;  
+  width: max-content;
+  max-width: 100%;
+  border-bottom: 1px solid var(--tertiary);
 `
 
 export const StyledBody = styled.p`
   font-size: 30px;
   font-weight: 100;
   text-align: center;
+  margin-bottom: 20px;
 `
 
-export const StyledName = styled.h2`
-  /* text-align: center; */
+export const StyledCommentName = styled.p`
+ font-size: 20px;
+`
+
+export const StyledCommentBody = styled.p`
+  font-weight: 100;
+  text-align: center;
+`
+
+export const StyledIcon = styled.div`
+  color: var(--white);
+  display: flex;
+  align-items: center;
+  background-color: var(--secundary);
+  border-radius: 50%;
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--tertiary);
+    scale: 1.06;
+    transition: all 0.4s ease-in-out;
+  }
 `
