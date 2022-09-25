c78f7068fe977b679d7a7b3bc8b4fbb9298343fc
Juliana Sinnott <julianasinnott@outlook.com>
2022-09-25 16:33:20 -0300

    refactor: transfer api request

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,31 @@
 import { Posts } from "../../components/Posts";
 import { Template } from "../../components/template";
+import { useEffect, useState } from "react"
+import api from "../../services/api";
 
 export function Home() {
-
+  const [posts, setPosts] = useState('') 
+  
+  useEffect(()=> {    
+    async function getPosts() {
+      try {
+        const result = await api.get('/posts')
+        setPosts(result.data)
+        console.log(result.data)
+      }
+      catch {
+        console.error(err);
+      }
+    }
+    getPosts()
+  },[])
+  console.log(posts)
   return(
     <>
     <Template>
-      <Posts />
+      {
+        posts && <Posts data={posts} />
+      }
     </Template>
     </>
   )
